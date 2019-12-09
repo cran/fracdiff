87c44329802f2c171459b1876a9bad85d2356096
Martin Maechler <maechler@stat.math.ethz.ch>
2019-12-09 14:00:02 +0000

    version 1.5-0

--- R/diffseries.R
+++ R/diffseries.R
@@ -1,7 +1,9 @@
-#### by Valderio Reisen  -- Dec.2005
+#### Fractional differentiation -- the inverse of fractional integration
+#### -------------------------- ----------------------------------------
 
+## by Valderio Reisen  -- Dec.2005--
 ## MM:  This is 'not optimal' -- and I may have better in ../filters.R ? <<< FIXME >>>
-diffseries <- function(x, d)
+diffseries0 <- function(x, d)
 {
     x <- as.data.frame(x)
     names(x) <- "series"
@@ -25,4 +27,50 @@ diffseries <- function(x, d)
     ## return numeric!
     ydiff
 }
+
+
+
+## From: alexios ghalanos <alexios@4dscape.com>
+## Date: Mon, 13 Jan 2014 19:58:48 +0000
+## To: <maechler@stat.math.ethz.ch>
+## Subject: fracdiff
+
+## Dear Martin,
+
+## Just a quick note, should it be of interest, that a very fast algorithm
+## for diffseries was recently published (1st version, 2013; 2nd: March 2014):
+## http://qed.econ.queensu.ca/working_papers/papers/qed_wp_1307.pdf‎ (ok, but wget fails!)
+## (MM: This is now published  ===> see ../man/diffseries.Rd)
+
+## Page 6 contains the R code and page 7 the benchmark timings.
+
+## Quick check (win 7 x64, R 3.02) shows a large performance boost (for 'large' n):
+#--------------------------------------
+## library(microbenchmark)
+## library(fracdiff)
+## memory.long <- fracdiff.sim(8000, d = 0.3)
+## mGPH <- fdGPH(memory.long$series)
+## Jensen and Nielsen code:
+## (slightly improved by MM)
+diffseries <- function(x, d) {
+    stopifnot((iT <- length(x)) >= 2)
+    x <- x - mean(x) ## <<-- Missing in J+N(2014)
+    np2 <- nextn(iT+iT - 1L)# changed from J+N: also factors 3 and 5
+    pad <- rep.int(0, np2-iT)
+    k <- seq_len(iT - 1L)
+    b <- c(1, cumprod((k - (d+1))/ k), pad)
+    ## ~= convolve(x, b, type = "filter") :
+    dx <- fft(fft(b) * fft(c(x, pad)), inverse =TRUE)[seq_len(iT)] / np2
+    Re(dx)
+}
+## microbenchmark(diffseries(memory.long$series, d = mGPH$d),
+## diffseries2(memory.long$series, d = mGPH$d))
+# Unit: milliseconds
+# diffseries  852.314992 (median)
+# diffseries2   3.181065 (median)
+#------------------------------------------------
+
+
+## Best Regards,
+## Alexios
 
--- tests/Valderio-ex.R
+++ tests/Valderio-ex.R
@@ -11,14 +11,26 @@ set.seed(8)
 mem.l2 <- fracdiff.sim(1024, d = 0.25)
 fdGPH(mem.l2$series)
 
-stopifnot(diffseries ( 1:20, d = 1) == c(-9.5, rep(1, 20-1)),
-          diffseries(-10:10, d = 0) == -10:10)
+diffserie0 <- fracdiff:::diffseries0 # the old slow for()-loop one
+stopifnot(exprs = {
+    all.equal(diffserie0(1:20, d = 1), c(-9.5, rep(1, 20-1)), tol = 1e-15)
+    all.equal(diffseries(1:20, d = 1), c(-9.5, rep(1, 20-1)), tol = 1e-13) # fft
+    all.equal(diffserie0(-10:10, d = 0), -10:10, tol = 1e-15)
+    all.equal(diffseries(-10:10, d = 0), -10:10, tol = 1e-13)
+    all.equal(diffserie0(-10:10, d = 1/2),
+              diffseries(-10:10, d = 1/2), tol = 1e-13) # see 4.3e-16 on 64b-Lnx
+})
+
 set.seed(123)
 ## example(diffseries)
 mem.l3 <- fracdiff.sim(80, d = 0.3)
 mGPH <- fdGPH(mem.l3$series)
-r <- diffseries(mem.l3$series, d = mGPH$d)
-print(r, digits = 4)
-print(acf(r)) # shouldn't show structure - ideally
+r0 <- diffserie0(mem.l3$series, d = mGPH$d)
+r. <- diffseries(mem.l3$series, d = mGPH$d)
+print(r0, digits = 4)
+all.equal(r0, r., tol = 0) # average rel.error
+stopifnot(all.equal(r0, r., tol = 1e-13))
+print(acf(r0)) #
+mtext("(shouldn't show structure - ideally)")
 
 cat("Time used: ", proc.time(),"\n") # for ``statistical reasons''
